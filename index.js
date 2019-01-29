f31139d592a3c299c021035518240740fcc0a2ff
pgswinter <tuananh.nguyen.freelance@gmail.com>
2019-01-29 07:43:54 +0700

    update graphql

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const { GraphQLServer } = require('graphql-yoga')
-
+const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('./generated/prisma-client');
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link
+}
 // 1
 // let links = [{
 //   id: 'link-0',
@@ -8,39 +18,39 @@ const { GraphQLServer } = require('graphql-yoga')
 // }]
 // let idCount = links.length
   
-const resolvers = {
-    Query: {
-      info: () => `This is the API of a Hackernews Clone`,
-      feed: (root, args, context, info) => {
-        return context.prisma.links()
-      },
-      // feed: () => links,
-    },
-    Mutation: {
-      // 2
-      post: (root, args, context) => {
-        return context.prisma.createLink({
-          url: args.url,
-          description: args.description,
-        })
-      },
-      // post: (parent, args) => {
-      //    const link = {
-      //     id: `link-${idCount++}`,
-      //     description: args.description,
-      //     url: args.url,
-      //   }
-      //   links.push(link)
-      //   return link
-      // }
-    },
-    // 3
-    Link: {
-      id: (parent) => parent.id,
-      description: (parent) => parent.description,
-      url: (parent) => parent.url,
-    }
-}
+// const resolvers = {
+//     Query: {
+//       info: () => `This is the API of a Hackernews Clone`,
+//       feed: (root, args, context, info) => {
+//         return context.prisma.links()
+//       },
+//       // feed: () => links,
+//     },
+//     Mutation: {
+//       // 2
+//       post: (root, args, context) => {
+//         return context.prisma.createLink({
+//           url: args.url,
+//           description: args.description,
+//         })
+//       },
+//       // post: (parent, args) => {
+//       //    const link = {
+//       //     id: `link-${idCount++}`,
+//       //     description: args.description,
+//       //     url: args.url,
+//       //   }
+//       //   links.push(link)
+//       //   return link
+//       // }
+//     },
+//     // 3
+//     Link: {
+//       id: (parent) => parent.id,
+//       description: (parent) => parent.description,
+//       url: (parent) => parent.url,
+//     }
+// }
 // const typeDefs = `
 //   type Query {
 //     info: String!
@@ -62,5 +72,12 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: 'schema.graphql',
     resolvers,
+    context: request => {
+      return {
+        ...request,
+        prisma,
+      }
+    },
+    // context: { prisma },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
